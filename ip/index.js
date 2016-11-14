bb1eb3ae72e869790ebf3220cbfcdb479a72f89f
Hanggi <hanggicrown@gmail.com>
2016-11-14 17:08:45 +0900

    add region

--- ip/index.js
+++ ip/index.js
@@ -13,10 +13,12 @@ var Ip = {
                     let bb = JSON.parse(body);
 
                     if (body) {
-                        console.log(chalk.cyan(`External_IP: ${bb.ip}`));
+                        console.log(chalk.green(`External_IP: ${bb.ip}`));
                         console.log(chalk.cyan(`City: ${bb.city}`));
+                        console.log(chalk.cyan(`Region: ${bb.region}`));
                         console.log(chalk.cyan(`Country: ${bb.country}`));
-                        console.log(chalk.cyan(`Org: ${bb.org}`));
+                        console.log(chalk.magenta(`Org: ${bb.org}`));
+                        console.log(chalk.blue(`Location: ${bb.loc}`));
                     }
                 }
             });
@@ -25,7 +27,7 @@ var Ip = {
         let self = this;
 
         console.log(chalk.yellow(`Hostname: ${self.hostname}`));
-        console.log(chalk.cyan(`IP: ${self.ip_v4}`));
+        console.log(chalk.green(`Internal_IP: ${self.ip_v4}`));
 
         self.external_ip();
     }
